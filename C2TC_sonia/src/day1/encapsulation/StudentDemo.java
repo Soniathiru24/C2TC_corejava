766958ea6d01a8ecb9e84c04b67bbf7e47df3e0a
HP <HP@DESKTOP-K12C64L>
2025-07-21 16:03:42 +0530

    encapsulation method

---
+++ C2TC_sonia/src/day1/encapsulation/StudentDemo.java
@@ -0,0 +1,11 @@
+package day1.encapsulation;
+
+public class StudentDemo {
+public static void main(String[]args) {
+	student ob =new student();
+	ob.setId(112);
+	ob.setName("soni");
+    System.out.println(ob.getId());
+    System.out.println(ob.getName());
+}
+}
---
+++ C2TC_sonia/src/day1/encapsulation/student.java
@@ -0,0 +1,22 @@
+package day1.encapsulation;
+
+public class student {
+private int id;
+private String name;
+public int getId() {
+	return id;
+}
+public void setId(int id) {
+	this.id = id;
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+@Override
+public String toString() {
+	return "student [id=" + ",name=" + name+"]";
+}
+}
