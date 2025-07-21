7a105b5a5429f6fd020b378ebcd2627b9cf42ecc
HP <HP@DESKTOP-K12C64L>
2025-07-21 20:53:51 +0530

    break statement

---
+++ C2TC_sonia/src/day2/breakDemo.java
@@ -0,0 +1,12 @@
+package day2;
+
+public class breakDemo {
+	public static void main(String[]args) {
+	for (int i = 1; i <= 10; i++) {
+        if (i == 8) {
+            break; // exit loop when i is 5
+        }
+        System.out.println(i);
+    }
+}
+}
