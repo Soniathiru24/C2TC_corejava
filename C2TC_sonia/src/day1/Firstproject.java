05107c47b0b8554e2d1e6f1ce94194454f6db08d
HP <HP@DESKTOP-K12C64L>
2025-07-21 12:25:45 +0530

    first java project

---
+++ C2TC_sonia/src/day1/Firstproject.java
@@ -0,0 +1,11 @@
+package day1;
+
+public class Firstproject {
+//this a method psvm 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("welcome to java class");
+
+	}
+
+}
