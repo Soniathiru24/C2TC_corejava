74a8c65e66e8d8649319ce213a1c1cc567242342
HP <HP@DESKTOP-K12C64L>
2025-07-21 19:30:14 +0530

    characters

---
+++ C2TC_sonia/src/day1/charDemo.java
@@ -0,0 +1,21 @@
+package day1;
+
+public class charDemo {
+	public static void main(String[] args) {
+		//assigning single character literal
+		char ch1 = 's';
+		System.out.println(ch1);
+		//assigning number to char
+		char ch = 68;
+		System.out.println(ch);
+		//assigning unicode to char
+		char sc='\u00A7';
+		System.out.println(sc);
+		//assigning representation
+		char sc2='\u20AC';
+		System.out.println(sc2);
+		//asci code representation
+		int a='Z';
+		System.out.println(a);
+	}
+}
