17b8cee611b3a844e359b238a0c49917bef0cce6
HP <HP@DESKTOP-K12C64L>
2025-07-23 21:41:48 +0530

    Encapsulation

---
+++ C2TC_sonia/src/day3/encapsulation/EncapsulationDemo.java
@@ -0,0 +1,24 @@
+package day3.encapsulation;
+
+public class EncapsulationDemo {
+public static void main(String[] args) {
+		
+		//object creation 
+		OopsConceptDemo obj = new OopsConceptDemo();
+		obj.setSerialNum(101);
+		obj.setName("Affu");
+		obj.setAge(20);
+		System.out.println(obj);
+		
+	}
+{
+	
+	//object creation 
+	OopsConceptDemo obj1 = new OopsConceptDemo();
+	obj1.setSerialNum(102);
+	obj1.setName("Anu");
+	obj1.setAge(21);
+	System.out.println(obj1);
+	
+}
+}
