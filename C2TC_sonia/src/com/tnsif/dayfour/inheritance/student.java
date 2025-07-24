e9ffa0180e734cdcf774fae05d4605052ae6c408
HP <HP@DESKTOP-K12C64L>
2025-07-24 22:20:10 +0530

    private constructor

---
+++ C2TC_sonia/src/com/tnsif/dayfour/inheritance/student.java
@@ -0,0 +1,12 @@
+package com.tnsif.dayfour.inheritance;
+
+public class student  extends Person{
+	
+	private int sid;
+	private String dept;
+	
+	void show() {
+		System.out.println("In Student Class");
+	}
+
+}
