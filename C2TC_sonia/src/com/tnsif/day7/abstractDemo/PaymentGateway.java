3b555f1430a053f6f8702ad01cefaa1cc4952d1c
HP <HP@DESKTOP-K12C64L>
2025-07-31 10:06:45 +0530

    day7

---
+++ C2TC_sonia/src/com/tnsif/day7/abstractDemo/PaymentGateway.java
@@ -0,0 +1,7 @@
+package com.tnsif.day7.abstractDemo;
+
+abstract class PaymentGateway {
+public void connectToPayment() {
+	System.out.println();
+}
+}
---
+++ C2TC_sonia/src/com/tnsif/day7/polymorphismDemo/overloading/Addition.java
@@ -0,0 +1,23 @@
+package com.tnsif.day7.polymorphismDemo.overloading;
+
+public class Addition {
+	public void add() {
+		System.out.println("Addition");
+	}
+	public int add(int a) {
+		return a;
+	}
+	public double add(double a) {
+		return a;
+	}
+	public int add(int a, int b) {
+		return a+b;
+	}
+	public void show (int n, String name) {
+		System.out.println(n+name);
+	}
+	public void show(String name,int n) {
+		System.out.println(name+n);
+	}
+
+}
